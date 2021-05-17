cc77ff5a4cadf06db969466eb53b3cdf7c533c60
StarGazer1915 <74311615+StarGazer1915@users.noreply.github.com>
2021-05-17 16:30:07 +0200

    Small change to Main

--- Adaptive Programming/2. Finite State Machine & Testing/src/sum2FSM/Main.java
+++ Adaptive Programming/2. Finite State Machine & Testing/src/sum2FSM/Main.java
@@ -4,10 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String input = "0123456789abcdefghijklmnopqrstuvwxyz";
-        String woord = "85amsterdamsegrachten24";
+        FSM fsm = new FSM("0123456789abcdefghijklmnopqrstuvwxyz");
+        fsm.startMachine("85amsterdamsegrachten24");
 
-        FSM fsm = new FSM(input);
-        fsm.startMachine(woord);
     }
 }

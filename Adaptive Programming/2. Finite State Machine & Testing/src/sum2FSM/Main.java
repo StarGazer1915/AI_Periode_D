c8c0bdf30116545d187a7268b1d5c605c1ee6c85
StarGazer1915 <74311615+StarGazer1915@users.noreply.github.com>
2021-05-26 11:17:59 +0200

    Added nodes and hashmaps for rework in Main

--- Adaptive Programming/2. Finite State Machine & Testing/src/sum2FSM/Main.java
+++ Adaptive Programming/2. Finite State Machine & Testing/src/sum2FSM/Main.java
@@ -1,11 +1,67 @@
 package sum2FSM;
 
+import Formatief2A.Node;
+
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        FSM fsm = new FSM("0123456789abcdefghijklmnopqrstuvwxyz");
-        fsm.startMachine("85amsterdamsegrachten24");
+        String input = "ADABFCEDEADEABFFC";
+
+        HashMap<String, Node> NodeMap0 = new HashMap<>();
+        HashMap<String, Node> NodeMap1 = new HashMap<>();
+        HashMap<String, Node> NodeMap2 = new HashMap<>();
+        HashMap<String, Node> NodeMap3 = new HashMap<>();
+        HashMap<String, Node> NodeMap4 = new HashMap<>();
+        HashMap<String, Node> NodeMap5 = new HashMap<>();
+
+        Node S0 = new Node("S0");
+        Node S1 = new Node("S1");
+        Node S2 = new Node("S2");
+        Node S3 = new Node("S3");
+        Node S4 = new Node("S4");
+        Node S5 = new Node("S5");
+
+        NodeMap0.put("A",S0);
+        NodeMap0.put("B",S1);
+        NodeMap0.put("C",S2);
+        NodeMap0.put("D",S3);
+        NodeMap0.put("E",S4);
+        NodeMap0.put("F",S5);
+
+        NodeMap1.put("A",S5);
+        NodeMap1.put("C",S2);
+        NodeMap1.put("D",S0);
+        NodeMap1.put("E",S1);
+        NodeMap1.put("F",S4);
+
+        NodeMap2.put("C",S5);
+        NodeMap2.put("D",S1);
+        NodeMap2.put("E",S3);
+        NodeMap2.put("F",S0);
+
+        NodeMap3.put("A",S3);
+        NodeMap3.put("B",S2);
+        NodeMap3.put("C",S1);
+        NodeMap3.put("D",S4);
+        NodeMap3.put("E",S0);
+        NodeMap3.put("F",S5);
+
+        NodeMap0.put("A",S4);
+        NodeMap0.put("B",S0);
+        NodeMap0.put("F",S3);
+
+        NodeMap0.put("A",S2);
+        NodeMap0.put("B",S3);
+        NodeMap0.put("C",S1);
+        NodeMap0.put("D",S4);
+        NodeMap0.put("E",S5);
+
+
+        //FSM fsm = new FSM(input);
+        //fsm.startMachine("85amsterdamsegrachten24");
 
     }
 }

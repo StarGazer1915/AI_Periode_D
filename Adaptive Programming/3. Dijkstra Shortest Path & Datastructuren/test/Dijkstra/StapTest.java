b2cf31223603a9921e692c035d031f7dd7a08ee4
StarGazer1915 <74311615+StarGazer1915@users.noreply.github.com>
2021-05-27 14:12:01 +0200

    Added stap tests.

--- Adaptive Programming/3. Dijkstra Shortest Path & Datastructuren/test/Dijkstra/StapTest.java
+++ Adaptive Programming/3. Dijkstra Shortest Path & Datastructuren/test/Dijkstra/StapTest.java
@@ -6,11 +6,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StapTest {
 
-    @Test
-    void getSize() {
-    }
+    private Treinrit T0 = new Treinrit("Amsterdam", "Rotterdam", 24);
+    private Rit R0 = new Rit("Amsterdam", "Utrecht", 28);
+    private Vlucht V0 = new Vlucht("Amsterdam", "Parijs", 428);
 
     @Test
-    void compareTo() {
+    void getSize() {
+        int A = T0.getSize();
+        int B = R0.getSize();
+        int C = V0.getSize();
+
+        assertEquals(24, A);
+        assertEquals(28, B);
+        assertEquals(428, C);
     }
 }

eb798e756d4f219aceb6113d75534092e5d56b3d
StarGazer1915 <74311615+StarGazer1915@users.noreply.github.com>
2021-05-27 12:38:33 +0200

    REIS: added getNaam(), created CompareTo() and getTotalSize() methods

--- Adaptive Programming/3. Dijkstra Shortest Path & Datastructuren/src/Dijkstra/Reis.java
+++ Adaptive Programming/3. Dijkstra Shortest Path & Datastructuren/src/Dijkstra/Reis.java
@@ -5,22 +5,50 @@ import java.util.PriorityQueue;
 public class Reis implements Comparable<Reis> {
 
     private String naam;
-    private PriorityQueue stappen;
+    private PriorityQueue<Stap> stappen;
 
     public Reis(String naam) {
         this.naam = naam;
+    }
+
+    public String getNaam() {
+        return naam;
     }
 
     public PriorityQueue getStappen() {
         return stappen;
     }
 
-    public void setStappen(PriorityQueue stappen) {
+    public void setStappen(PriorityQueue<Stap> stappen) {
         this.stappen = stappen;
     }
 
     @Override
-    public int compareTo(Reis o) {
-        return 0;
+    public int compareTo(Reis Bravo) {
+        int A = stappen.stream().mapToInt(stap -> stap.getSize()).sum();
+        int B = Bravo.getTotalSize();
+        if ( B > A ) {
+            System.out.println("-----| " + naam + " is korter/kleiner dan " + Bravo.getNaam() + " |-----" +
+                    "\n" + naam + " = " + A +
+                    "\n" + Bravo.getNaam() + " = " + B + "\n");
+            return 1;
+        } else {
+            System.out.println("-----| " + naam + " is langer/groter dan " + Bravo.getNaam() + " |-----" +
+                    "\n" + naam + " = " + A +
+                    "\n" + Bravo.getNaam() + " = " + B + "\n");
+            return -1;
+        }
+    }
+
+    public int getTotalSize() {
+        return stappen.stream().mapToInt(stap -> stap.getSize()).sum();
+    }
+
+    @Override
+    public String toString() {
+        return "Reis{" +
+                "naam='" + naam + '\'' +
+                ", stappen=" + stappen +
+                '}';
     }
 }

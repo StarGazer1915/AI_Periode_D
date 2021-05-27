2856cb56661b54a05e9a4330c76c349ff16eb619
StarGazer1915 <74311615+StarGazer1915@users.noreply.github.com>
2021-05-27 12:41:16 +0200

    VLUCHT: changed float to int, created getSize() and toString() method.

--- Adaptive Programming/3. Dijkstra Shortest Path & Datastructuren/src/Dijkstra/Vlucht.java
+++ Adaptive Programming/3. Dijkstra Shortest Path & Datastructuren/src/Dijkstra/Vlucht.java
@@ -2,14 +2,26 @@ package Dijkstra;
 
 public class Vlucht extends Stap {
 
-    private final float euro;
+    private final int euro;
 
-    public Vlucht(String locA, String locB, float euro) {
+    public Vlucht(String locA, String locB, int euro) {
         super(locA, locB);
         this.euro = euro;
     }
 
-    public float getEuro() {
+    public int getEuro() {
         return euro;
+    }
+
+    @Override
+    public int getSize() {
+        return euro;
+    }
+
+    @Override
+    public String toString() {
+        return "Vlucht{" +
+                "euro=" + euro +
+                '}';
     }
 }
